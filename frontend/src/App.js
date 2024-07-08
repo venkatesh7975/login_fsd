f8d0351fe5edabd5113550f71d5c87307aa3041b
venkatesh7975 <venkateshmoyya7975@gmail.com>
2024-07-08 16:21:41 +0530

    second commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 export default function App() {
+  const [username, setUserName] = useState("");
+  const [password, setPassWord] = useState("");
+  const onRegister = () => {
+    alert("register clicked");
+  };
+  const onLogin = () => {
+    alert("login clicked");
+  };
+
   return (
     <div className="container">
-      <input placeholder="username" type="text" />
-      <input placeholder="password" type="password" />
+      <input
+        placeholder="username"
+        type="text"
+        onChange={(event) => setUserName(event.target.value)}
+      />
+      <p>{username}</p>
+      <input
+        placeholder="password"
+        type="password"
+        onChange={(event) => setPassWord(event.target.value)}
+      />
+      <p>{password}</p>
       <div>
-        <button>Register</button>
-        <button>Login</button>
+        <button onClick={onRegister}>Register</button>
+        <button onClick={onLogin}>Login</button>
       </div>
     </div>
   );
